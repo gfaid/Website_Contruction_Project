f229ba825dea75bed83373c305b756e62ba4d091
gfaid <135048942+gfaid@users.noreply.github.com>
2023-05-31 04:08:38 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const title_elm = document.getElementById("title");
-
-title_elm.addEventListener("mouseover", e => {
-    e.target.value = "I'm Choi!"
-});
-title_elm.addEventListener("mouseout", e => {
-    e.target.value = "Who Is He?"
-});
